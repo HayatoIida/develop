f7b706c4d4f77770b6505fa571928d9a4e1dc683
albus1881 <45604054+albus1881@users.noreply.github.com>
2018-12-05 16:21:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,5 @@
 import discord # インストールした discord.py
 import random
-import math
 
 client = discord.Client() # 接続に使用するオブジェクト
 #locaton = ['ジャンク・ジャンクション', 'ホーンテッド・ヒルズ', 'プレザント・パーク', 'スノビー・ショア', 'バイキング・ビレッジ', 'ティルテッド・タワー', 'グリーシー・グローブ', 'シフティ・シャフト', 'レイジー・リンクス', 'トマト・テンプル', 'リスキー・リールズ', 'ウェイリング・ウッズ', 'ダスティ・ディポット', 'ソルティ・スプリングス', 'フェイタル・フィールド', 'リテイル・ロー', 'フラッシュ・ファクトリー', 'ラッキー・ランディング', 'パラダイス・パームズ', 'レイジーリンクスの西のモーテル', 'ティルテッドタワー西のおいしめの土地', 'フラッシュファクトリー北東のおいしめの土地', 'ロンリー・ロッジ', 'サーキット', '砂漠南のおいしめの土地', 'コンテナのとこ']
@@ -21,16 +20,17 @@ async def on_message(message):
         t1 = client.get_channel('519678340232249344')
         t2 = client.get_channel('519678398214176789')
         mem = c.voice_members
-        length = math.ceil( len(mem) / 2 )
+        length = -(- len(mem) // 2 )
         for x in mem:
+            await client.send_message(message.channel, x.display_name)
             t1len = len(t1.voice_members)
             t2len = len(t2.voice_members)
             if t1len < length and t2len < length:
-                await client.move_member(x, random.choice([t1,t2]))
+                client.move_member(x, random.choice([t1,t2]))
             elif t1 < length:
-                await client.move_member(x, t1)
-            else t2 < length:
-                await client.move_member(x, t2)
+                client.move_member(x, t1)
+            elif t2 < length:
+                client.move_member(x, t2)
 
     if message.content.startswith('!fgather'):
         c = client.get_channel('489330400032849940')
@@ -38,10 +38,10 @@ async def on_message(message):
         t2 = client.get_channel('519678398214176789')
         t1mem = t1.voice_members
         for x in t1mem:
-            await client.move_member(x, c)
+            client.move_member(x, c)
         t2mem = t2.voice_members
         for x in t2mem:
-            await client.move_member(x, c)
+            client.move_member(x, c)
 
     if message.content.startswith('!fcommand'):
         if client.user == message.author:
